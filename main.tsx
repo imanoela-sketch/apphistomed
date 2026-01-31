0ee179f6b4d99ed874ab3c7f53ea3c65edc3ef54
imanoela-sketch <imanoela@gmail.com>
2026-01-31 10:55:20 -0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-// Este código liga o seu arquivo App.tsx ao seu index.html
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
