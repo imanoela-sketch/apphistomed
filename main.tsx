b495db4e1c047af9e922a405d4faebef510fc6c4
imanoela-sketch <imanoela@gmail.com>
2026-01-31 11:42:49 -0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app'; // Agora apontando para 'app' minúsculo
 import './index.css';
 
 const rootElement = document.getElementById('root');
